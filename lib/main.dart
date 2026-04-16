db8783c33ccd85848bd0313ed63ea3ce3bdaad10
alaa <alaahefni09@gmail.com>
2026-04-16 21:58:50 +0200

    Första commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/pages/main_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Receptsök',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainView(),
+    );
+  }
+}
---
+++ lib/pages/main_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(children: [_controlPanel(context), _recipeArea(context)],),
+    );
+  }
+  
+  Widget _controlPanel(context, {double width = 320}) {
+    return Container(
+      width: width, 
+      color: const Color.fromARGB(255, 241, 178, 227),
+      child: Column(
+        children: [
+          Text("Receptsök"),
+          Text("Hitta ett recept som passar genom att ändra inställningarna nedanför")
+        ],),
+    );
+  }
+  
+  
+  
+  
+  
+  
+  
+  Widget _recipeArea(context) {
+    return Expanded(
+      child: Container(color: const Color.fromARGB(77, 212, 90, 188)));
+  }
+}
